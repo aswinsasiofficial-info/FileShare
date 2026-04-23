c38dc2b8d16d0294e87f9931ef4cf7bf4d9639bc
aswinsasiofficial-info <aswinsasiofficial.in@gmail.com>
2026-04-23 21:10:16 +0530

    admin

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='accounts:login'), name='logout'),
+    path('users/', views.user_list, name='user_list'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib import messages
+from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.models import User
+from django.db.models import Count
 from .forms import UserRegistrationForm
 
 def register(request):
@@ -14,3 +17,8 @@ def register(request):
     else:
         form = UserRegistrationForm()
     return render(request, 'accounts/register.html', {'form': form})
+
+@user_passes_test(lambda u: u.is_superuser)
+def user_list(request):
+    users = User.objects.annotate(file_count=Count('files')).order_by('-date_joined')
+    return render(request, 'accounts/user_list.html', {'users': users})
