4f7ac1a95243ab14f4b562bcf33bd5766ba22c93
aswinsasiofficial-info <aswinsasiofficial.in@gmail.com>
2026-04-23 21:20:01 +0530

    admin2

--- accounts/forms.py
+++ accounts/forms.py
@@ -9,9 +9,16 @@ class UserRegistrationForm(UserCreationForm):
         model = User
         fields = ['email']
 
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(username=email).exists() or User.objects.filter(email=email).exists():
+            raise forms.ValidationError("A user with this email already exists.")
+        return email
+
     def save(self, commit=True):
         user = super().save(commit=False)
         user.username = self.cleaned_data['email']
+        user.email = self.cleaned_data['email']
         if commit:
             user.save()
         return user
